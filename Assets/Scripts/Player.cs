0376bf3a333ea25b89815a54eb1e9d5ec57839c3
Treidex <treidexwastaken@gmail.com>
2021-12-20 09:18:10 -0800

    dash

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Player: MonoBehaviour
 {
 	public Rigidbody2D rb;
+	public float baseSpeed;
+	public float dashSpeed;
+	[HideInInspector]
 	public float speed;
 
 	void FixedUpdate()
 	{
-		float dx = Input.GetAxisRaw("Horizontal") * speed;
+		float speed = baseSpeed;
+		if (Input.GetKey(KeyCode.LeftShift))
+			spd = dashSpeed;
+		float dx = Input.GetAxisRaw("Horizontal") * spd;
 		rb.AddForce(new Vector2(dx, 0), ForceMode2D.Impulse);
 	}
 }
